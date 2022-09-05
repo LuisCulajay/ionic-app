b22700cfa38b1c1486077b4e0948482ba87462a9
Luis Culajay <3548487470101@ingenieria.usac.edu.gt>
2022-09-05 08:33:36 -0600

    app compilada funcionando

---
+++ pruebas/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'pruebas',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
